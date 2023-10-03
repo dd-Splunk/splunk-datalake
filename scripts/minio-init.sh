cdd98c62e7005263703e66328a535a5d4770606b
dd-Splunk <ddessy@splunk.com>
2023-10-03 09:13:07 +0200

    Add extra bucket for smartstore

--- scripts/minio-init.sh
+++ scripts/minio-init.sh
@@ -4,26 +4,28 @@ set +o history
 
 # Server
 ALIAS=local
+PROT=https
 
 # Wait for Minio server to be up
 echo "Waiting for ${MINIO_SERVER} ..."
-until $(curl --output /dev/null -k --silent --head --fail https://${MINIO_SERVER}:9000/minio/health/live ); do
+until $(curl --output /dev/null -k --silent --head --fail ${PROT}://${MINIO_SERVER}:9000/minio/health/live ); do
     printf '.'
     sleep 1
 done
 echo "${MINIO_SERVER} is up." 
 
 # Create alias to local minio
-mc  alias set --insecure ${ALIAS} https://${MINIO_SERVER}:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
+mc  alias set --insecure ${ALIAS} ${PROT}://${MINIO_SERVER}:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 
 # Create service account 
-mc admin user --insecure  svcacct add                       \
-   --access-key "${AWS_ACCESS_KEY_ID}"          \
+mc admin user --insecure  svcacct add       \
+   --access-key "${AWS_ACCESS_KEY_ID}"      \
    --secret-key "${AWS_SECRET_ACCESS_KEY}"  \
    ${ALIAS} admin
 
 # Create bucket 
 mc mb --insecure ${ALIAS}/${MINIO_BUCKET}
+mc mb --insecure ${ALIAS}/vol01
 
 set -o history 
 exit 0
