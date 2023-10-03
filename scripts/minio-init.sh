af7bec0d8e812cf965c37c5b4421ec9797f137fe
dd-Splunk <ddessy@splunk.com>
2023-10-03 12:06:33 +0200

    removing health check

--- scripts/minio-init.sh
+++ scripts/minio-init.sh
@@ -7,15 +7,15 @@ ALIAS=local
 PROT=https
 
 # Wait for Minio server to be up
-echo "Waiting for ${MINIO_SERVER} ..."
-until $(curl --output /dev/null -k --silent --head --fail ${PROT}://${MINIO_SERVER}:9000/minio/health/live ); do
-    printf '.'
-    sleep 1
-done
-echo "${MINIO_SERVER} is up." 
+# echo "Waiting for ${MINIO_SERVER} ..."
+# until $(curl --output /dev/null -k --silent --head --fail ${PROT}://${MINIO_SERVER}:9000/minio/health/live ); do
+#     printf '.'
+#     sleep 1
+# done
+# echo "${MINIO_SERVER} is up." 
 
 # Create alias to local minio
-mc  alias set --insecure ${ALIAS} ${PROT}://${MINIO_SERVER}:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
+mc alias set --insecure ${ALIAS} ${PROT}://${MINIO_SERVER}:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 
 # Create service account 
 mc admin user --insecure  svcacct add       \
