f0499cd53ecd11ba166ba4e4b9edf213dad12edb
dd-Splunk <ddessy@splunk.com>
2023-10-25 16:22:58 +0200

    send to both splunk and minio

--- configs/docker/set-ingest-actions.sh
+++ configs/docker/set-ingest-actions.sh
@@ -14,4 +14,4 @@ echo "Add rules"
 
 curl -k -u admin:$SPLUNK_PASSWORD --location 'https://so1:8089/services/data/ingest/rulesets//ruleset_heure?output_mode=json' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'rules=[{"name":"r0","action":"set_index","expr":"\"cust0\"","cond":{"type":"eval","expr":"cust==\"customer-unlimited\""}},{"name":"r1","action":"set_index","expr":"\"cust1\"","cond":{"type":"eval","expr":"cust==\"customer-normal\""}},{"name":"r2","action":"set_index","expr":"\"cust2\"","cond":{"type":"eval","expr":"cust==\"customer-double\""}},{"name":"rroute","action":"route","clone":false,"dest":"rfs:minio","cond":{"type":"eval","expr":"true()"}}]'
+--data-urlencode 'rules=[{"name":"r0","action":"set_index","expr":"\"cust0\"","cond":{"type":"eval","expr":"cust==\"customer-unlimited\""}},{"name":"r1","action":"set_index","expr":"\"cust1\"","cond":{"type":"eval","expr":"cust==\"customer-normal\""}},{"name":"r2","action":"set_index","expr":"\"cust2\"","cond":{"type":"eval","expr":"cust==\"customer-double\""}},{"name":"rroute","action":"route","clone":false,"dest":"rfs:minio,_splunk_","cond":{"type":"eval","expr":"true()"}}]'
