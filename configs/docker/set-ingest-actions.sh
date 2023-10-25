d7ebf9e5b7b557e4e96f97c08398f651627e31a5
dd-Splunk <ddessy@splunk.com>
2023-10-25 19:44:37 +0200

    Create ruleset and rules at once

--- configs/docker/set-ingest-actions.sh
+++ configs/docker/set-ingest-actions.sh
@@ -16,10 +16,10 @@ curl -k -u admin:$SPLUNK_PASSWORD --location 'https://so1:8089/services/data/ing
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'name=ruleset_heure' \
 --data-urlencode 'sourcetype=heure' \
---data-urlencode 'rules=[]'
-
-echo "Add rules"
-
-curl -k -u admin:$SPLUNK_PASSWORD --location 'https://so1:8089/services/data/ingest/rulesets//ruleset_heure' \
---header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'rules=[{"name":"r0","action":"set_index","expr":"\"cust0\"","cond":{"type":"eval","expr":"cust==\"customer-unlimited\""}},{"name":"r1","action":"set_index","expr":"\"cust1\"","cond":{"type":"eval","expr":"cust==\"customer-normal\""}},{"name":"r2","action":"set_index","expr":"\"cust2\"","cond":{"type":"eval","expr":"cust==\"customer-double\""}},{"name":"rroute","action":"route","clone":false,"dest":"rfs:minio,_splunk_","cond":{"type":"eval","expr":"true()"}}]'
+
+# curl -k -u admin:$SPLUNK_PASSWORD --location 'https://so1:8089/services/data/ingest/rulesets//ruleset_heure' \
+# --header 'Content-Type: application/x-www-form-urlencoded' \
+# --data-urlencode 'rules=[{"name":"r0","action":"set_index","expr":"\"cust0\"","cond":{"type":"eval","expr":"cust==\"customer-unlimited\""}},{"name":"r1","action":"set_index","expr":"\"cust1\"","cond":{"type":"eval","expr":"cust==\"customer-normal\""}},{"name":"r2","action":"set_index","expr":"\"cust2\"","cond":{"type":"eval","expr":"cust==\"customer-double\""}},{"name":"rroute","action":"route","clone":false,"dest":"rfs:minio,_splunk_","cond":{"type":"eval","expr":"true()"}}]'
+
+echo "Rules added"
