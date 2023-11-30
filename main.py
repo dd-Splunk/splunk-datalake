7c23477166eac7f7712cbf4f8068501c786bc6e9
dd-Splunk <ddessy@splunk.com>
2023-11-30 18:51:13 +0100

    listing objects

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import urllib3
+
 urllib3.disable_warnings()
 
 from minio import Minio
 from minio.error import S3Error
 
-endpoint=os.getenv('MINIO_URL', "localhost:9000" )
-access_key=os.getenv('MINIO_ACCESS_KEY', "admin")
-secret_key=os.getenv('MINIO_SECRET_KEY', "Password$")
+endpoint = os.getenv("MINIO_URL", "localhost:9000")
+access_key = os.getenv("MINIO_ACCESS_KEY", "admin")
+secret_key = os.getenv("MINIO_SECRET_KEY", "Password$")
 
 
 client = Minio(
@@ -23,5 +24,10 @@ buckets = client.list_buckets()
 print(buckets)
 
 for bucket in buckets:
-    print(bucket)
+    print(bucket.name, bucket.creation_date)
+    objects = client.list_objects(bucket.name, recursive=True)
+    for obj in objects:
+        print(obj.object_name)
+
+
 
