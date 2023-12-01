b500bac04b849d0684caee9aadefa14224ba2579
dd-Splunk <ddessy@splunk.com>
2023-12-01 10:56:17 +0100

    decode gz

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import urllib3
 
 urllib3.disable_warnings()
 
+from datetime import date
 from minio import Minio
 from minio.error import S3Error
+
+import zlib
+import ndjson
+import jsonlines
+
+
+def stream_gzip_decompress(stream):
+    dec = zlib.decompressobj(32 + zlib.MAX_WBITS)  # offset 32 to skip the header
+    for chunk in stream:
+        rv = dec.decompress(chunk)
+        if rv:
+            yield rv
+    if dec.unused_data:
+        # decompress and yield the remainder
+        yield dec.flush()
+
 
 endpoint = os.getenv("MINIO_URL", "localhost:9000")
 access_key = os.getenv("MINIO_ACCESS_KEY", "admin")
@@ -19,15 +36,29 @@ client = Minio(
     cert_check=False,
 )
 
+thisday = date.today()
+# filter on today's buckets
+bucket_prefix = (
+    f"year={thisday.year:0{4}}/month={thisday.month:0{2}}/day={thisday.day:0{2}}/"
+)
+
+
 # List buckets
 buckets = client.list_buckets()
-print(buckets)
 
 for bucket in buckets:
     print(bucket.name, bucket.creation_date)
-    objects = client.list_objects(bucket.name, recursive=True)
+    objects = client.list_objects(bucket.name, prefix=bucket_prefix, recursive=True)
     for obj in objects:
         print(obj.object_name)
+        try:
+            response = client.get_object(bucket.name, obj.object_name)
+            # Read data from response.
+            items = response.read(decode_content=True)
+            lines=zlib.decompress(items, 15 + 32)
+            print(lines)
 
-
-
+        finally:
+            response.close()
+            response.release_conn()
+        print("---")
