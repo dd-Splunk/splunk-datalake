8e5d8fc2d829e9d71c7c1b9cb605983354c217ab
dd-Splunk <ddessy@splunk.com>
2023-12-01 17:44:43 +0100

    Adding HEC output

---
+++ hec.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+requests.packages.urllib3.disable_warnings()
+
+
+def send_to_hec(event) -> int:
+    hec_host = "https://localhost:8088"
+    hec_token = "abcd-1234-efgh-5678"
+    hec_endpoint = "/services/collector/event"
+
+    url = hec_host + hec_endpoint
+    headers = {"Authorization": "Splunk " + hec_token}
+
+    # https://medium.com/@rysartem/sending-data-to-splunk-hec-in-a-right-way-4a84af3c44e2
+    response = requests.post(
+        url=url,
+        headers=headers,
+        data=json.dumps(event, ensure_ascii=False).encode("utf-8"),
+        verify=False,
+    )
+    return response.status_code
+
+
+if __name__ == "__main__":
+    # Test data
+    event = {
+        "time": 1701433088,
+        "event": "Current Time = 13:18:08\n",
+        "host": "uf2",
+        "source": "/opt/splunkforwarder/etc/apps/double-speed/bin/heure.py",
+        "sourcetype": "heure",
+        "index": "cust2",
+        "fields": {"cust": "customer-double"},
+    }
+
+    status = send_to_hec(event=event)
+    print(f"Event sent, status {status}")
